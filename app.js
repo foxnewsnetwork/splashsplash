c6a340b0906c60a0f8cfc607e2d452a450a0afd4
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-08-29 19:04:34 -0700

    one-shot this thing

--- app.js
+++ app.js
@@ -1,33 +1,69 @@
+/**
+* DB
+*/
+var mongoose = require( 'mongoose' );
+var db = mongoose.connect( "mongodb://localhost/mysplashpage" );
+
+var Schema = mongoose.Schema
+	, ObjectId = Schema.ObjectId;
+
+var ProjectIconSchema = new Schema({
+	image : String ,
+	target : String ,
+	x : Number ,
+	y : Number ,
+	width : Number ,
+	height : Number ,
+	body : String
+}); // ProjectIcon
+
+var ProjectIcon = db.model( "ProjectIcon", ProjectIconSchema );
 
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-var app = express();
+server.listen(3123);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+io.sockets.on( "connection", function(socket) { 
+	ProjectIcon.find( {}, function(err, docs) { 
+		socket.emit( "project icons down", docs );
+		if ( err ) {
+			socket.emit( "error down", err );
+			console.log( err );
+		} // if err
+	} ); // find
+	
+	socket.on( "remove project icon up", function(id) { 
+		ProjectIcon.findOneAndRemove( { _id : id }, function(err) { 
+			console.log( err );
+			socket.emit( "remove project icon down", err );
+		} ); // findOneAndRemove
+	} ); // remove project icon up
+	
+	socket.on( "project icon up", function(icon) { 
+		if( icon['_id'] ) {
+			var id = icon['_id']; 
+			delete icon['_id'];
+			ProjectIcon.findOneAndUpdate( { _id : id }, icon, function(err, doc) { 
+				if ( err ) {
+					socket.emit( "error down", err );
+					console.log( err );
+				} // if err
+				socket.emit( "project icon down", doc )
+			} ); // findOne
+		} // if icon
+		else { 
+			(new ProjectIcon(icon) ).save( function(err) { 
+				if ( err ) {
+					socket.emit( "error down", err );
+					console.log( err );
+				} // if err
+			} ); // save
+		} // else
+	} ); // on project icon up
+} ); // on connection
